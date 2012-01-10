4c72167cc2dee1e8eae73cbe157c3c4e5dc63c5d
Peter Boyd <marschronicles@fastmail.fm>
2012-01-10 02:24:13 +0200

    New version 0.2 [Fixed] UTF-8 response processing [Added] Better error handling [Added] Better HTML format of DOC_ID.html [Added] Raw json response save to DOC_ID.raw [Added] Moving the deleted files to GROUP_ID/deleted folder [Added] Color output

--- group-docs-backup.py
+++ group-docs-backup.py
@@ -28,18 +28,31 @@
 #
 
 """This program downloads all available from a selected FB Group
-   documents in the script folder, so they can be backed up or added and managed by
-   git, hg, svn etc
+   documents in the script folder, so they can be backed up or added and
+   managed by git, hg, svn etc
+
+   Tree content:
+        GROUP_ID/ - id folder of the group
+            |
+            |--deleted/ - folder to keep already deleted Docs (raw and html)
+            |
+            |--DOC_ID.raw - FB json raw response for the Doc
+            |
+            |--DOC_ID.html - FB response for the Doc in HTML, so it can be
+                             copied/pasted online conveniently
 """
 
 
 import os
 import sys
+import glob
+import shutil
+import codecs
+
 import json
 import urllib2
 import BaseHTTPServer
 import webbrowser
-import string
 
 from urlparse import urlparse, parse_qs
 from urllib import urlencode
@@ -47,35 +60,95 @@ from urllib import urlencode
 __authors__ = "Peter Boyd"
 __copyright__ = "Copyright 2012, Peter Boyd"
 __license__ = "GPL"
-__version__ = "0.1"
+__version__ = "0.2"
 __maintainer__ = "Peter Boyd"
 __status__ = "Development"
 
 
+# You can edit the group IDs if you want
+GROUPS_ID = ('2204510018','228135853929943') # Linux and Linux (tech support) groups
+
+
+# No need to edit anything below here
 APP_ID = '205214459571218'
 APP_NAME = "Group Docs Backup"
-GROUPS_ID = ('2204510018','228135853929943') # Linux and Linux (tech support) groups
+ACCESS_TOKEN = None
+AUTH_SCOPE = ['user_groups', 'offline_access']
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8080
 REDIRECT_URI = 'http://%s:%s/' % (SERVER_IP,SERVER_PORT)
-ACCESS_TOKEN = None
-AUTH_SCOPE = ['user_groups', 'offline_access']
+
 LOCAL_FILE = os.environ["HOME"] + '/.fb_access_token'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+COLORS = dict(
+        list(zip([
+            'FAIL',
+            'OK',
+            'WARN',
+            ],
+            list(range(31,34))
+            ))
+        )
+RESET = '\033[0m'
+
+HTML = """<!DOCTYPE html>
+<html>
+    <head>
+        <title>%s</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+    </head>
+    <body>
+        <b>TITLE OF THE DOCUMENT (just copy/paste it):</b>
+        <div style="border: 2px solid; padding: 10px; margin: 10px 50px 50px;">
+             %s
+        </div>
+
+        <b>CONTENT OF THE DOCUMENT (just copy/paste it):</b>
+        <div style="border: 2px solid; padding: 10px; margin: 10px 50px;">
+            %s
+        </div>
+    </body>
+</html>
+"""
 
 __all__ = [
     'help',
     'authenticate',
     'graph',
+    'GROUPS_ID',
     'APP_ID',
     'APP_NAME',
-    'GROUPS_ID',
-    'SERVER_IP',
-    'SERVER_PORT',
     'ACCESS_TOKEN',
     'AUTH_SCOPE',
+    'SERVER_IP',
+    'SERVER_PORT',
     'SCRIPT_DIR',
-    'LOCAL_FILE']
+    'LOCAL_FILE'
+    'COLORS',
+    'RESET',
+    'HTML']
+
+def colored(text, color=None):
+    """Colorize and bold text.
+
+    Available text colors:
+        FAIL (red), OK (green), WARN (yellow), no color is bold only
+
+    Example:
+        colored('TEST STRING', 'OK') -> bold green
+        colored('TEST STRING') -> bold
+    """
+    if os.getenv('ANSI_COLORS_DISABLED') is None:
+        if color is not None:
+            text = '\033[1;%dm%s' % (COLORS[color], text)
+        else:
+            text = '\033[1m%s' % text
+
+        text += RESET
+
+    return text
 
 def _get_url(path, args=None):
     args = args or {}
@@ -85,7 +158,6 @@ def _get_url(path, args=None):
     return 'https://graph.facebook.com'+str(path)+'?'+urlencode(args)
 
 class _RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-
     def do_GET(self):
         global ACCESS_TOKEN
         self.send_response(200)
@@ -104,7 +176,7 @@ class _RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 f.close()
                 os.chmod(LOCAL_FILE, 0700)
             except:
-                print  "Error creating: %s" % LOCAL_FILE
+                print  colored("Error creating: %s" % LOCAL_FILE, 'FAIL')
                 print  sys.exc_info()
             else:
                 self.wfile.write('<html><head><title>Login success</title></head>'
@@ -118,10 +190,10 @@ class _RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                              '</head></html>')
 
 def authenticate():
-    """Authenticate with Facebook so you can make API calls that require auth.
+    """Authenticate with Facebook, so you can make API calls that require auth.
 
     Alternatively you can just set the ACCESS_TOKEN global variable in this
-    module to an access token you get from facebook.
+    module to an access token you get from Facebook.
 
     If you want to request certain permissions, set the AUTH_SCOPE global
     variable to the list of permissions you want.
@@ -133,11 +205,11 @@ def authenticate():
         try:
             data = json.loads(open(LOCAL_FILE).read())
         except:
-            print "Local Facebook Authorisation file problem. We need re-authentication..."
+            print colored("Local Facebook Authorisation file problem. We need re-authentication...", 'FAIL')
             try:
                 os.remove(LOCAL_FILE)
             except:
-                print  "Error deleting: %s" % LOCAL_FILE
+                print  colored("Error deleting: %s" % LOCAL_FILE, 'FAIL')
                 print  sys.exc_info()
 
         else:
@@ -168,7 +240,7 @@ def graph(groupID, path, params=None):
       >>> graph('/me', {'fields':'id,name'})
 
     """
-    print "\nGeting Docs data for Group ID: %s..." % groupID
+    print "\nGeting Docs data for Group ID: %s..." % colored(groupID)
 
     try:
         response = urllib2.urlopen(_get_url(path, args=params))
@@ -195,9 +267,12 @@ def main():
 
     It does (in order of appearance):
         - authenticate
-        - create group ID folders
         - get all the docs data from each group
-        - writes every doc with the name ID.html
+        - create group ID folders if needed
+        - writes every doc with the name ID.raw, so we have raw json data
+        - writes every doc with the name ID.html, so we have convenient format
+        - checks if any Docs were deleted since last backup. If yes -> creates
+          a deleted folder and move the file/s there
         - prompts for adding to git, hg etc
     """
     global ACCESS_TOKEN
@@ -205,54 +280,126 @@ def main():
     authenticate()
 
     for gid in GROUPS_ID:
-        # We use Group ID for the folder names because Group Name may change
-        GROUP_DIR = SCRIPT_DIR + '/' + gid
-        if not os.path.exists(GROUP_DIR):
-            try:
-                os.makedirs(GROUP_DIR)
-            except:
-                print  "Error creating Group ID folder: %s" % GROUP_DIR
-                print  sys.exc_info()
-            else:
-                print "\nCreated folder for Group ID: %s" % gid
-
-        json_data = graph(gid, "/%s/docs" % gid, {'fields':'id,subject,message'})
+        json_data = graph(gid, "/%s/docs" % gid)
 
         while 'error' in json_data.keys():
-            print "Authorisation problem: " + str(json_data['error'])
-            print "Trying to reauthorise the application on Facebook..."
+            if str(json_data['error']['type']) != 'OAuthException': # OR == 'GraphMethodException'
+                break
+
+            print colored("Authorisation problem: ", 'FAIL') + colored(str(json_data['error']))
+            print colored("Trying to reauthorise the application on Facebook...", 'WARN')
 
             try:
                 os.remove(LOCAL_FILE)
             except:
-                print  "Error deleting: %s" % LOCAL_FILE
+                print  colored("Error deleting: %s" % LOCAL_FILE, 'FAIL')
                 print  sys.exc_info()
 
             ACCESS_TOKEN = None
             authenticate()
-            json_data = graph(gid, "/%s/docs" % gid, {'fields':'id,subject,message'})
+            json_data = graph(gid, "/%s/docs" % gid)
+
+        if not 'data' in json_data.keys():
+            print colored("There is a problem with Group ID: %s" % gid, 'FAIL')
+            print colored("Go to Facebook and check its ID please.", 'WARN')
+            print colored("Link to the group: ", 'WARN') + colored('https://www.facebook.com/groups/%s' % gid)
+            continue
 
         doc = json_data['data']
 
         if not doc: # If data is empty, it means we are not allowed to read group docs
-            print "You are not Authorised to read the Docs section of the Group with ID: %s" % gid
-            print "No documents will be downloaded for this group!"
-            print "Go to Facebook and join the group first please."
+            print colored("No Docs or not Authorised to read the Docs section of the Group with ID: %s" % gid, 'FAIL')
+            print colored("No documents will be downloaded for this group!", 'WARN')
+            print colored("Go to Facebook and check or join the group first please.", 'WARN')
+            print colored("Link to the group: ", 'WARN') + colored('https://www.facebook.com/groups/%s' % gid)
 
+        else:
+            # We use Group ID for the folder names because Group Name may change
+            GROUP_DIR = SCRIPT_DIR + '/' + gid
+            if not os.path.exists(GROUP_DIR):
+                try:
+                    os.makedirs(GROUP_DIR)
+                except:
+                    print  colored("Error creating Group ID folder: %s" % GROUP_DIR, 'FAIL')
+                    print  sys.exc_info()
+                else:
+                    print colored("\nCreated folder for Group ID: ", 'OK') + colored(gid)
 
-        for entry in doc:
-            try:
-                f = open(GROUP_DIR + '/' + entry['id'] + '.html','w')
-                f.write(u'TITLE: '+ entry['subject'] + u'\r\n\r\n<br><br>' + string.replace(entry['message'].encode('UTF-8'), '\xc2\xa0', ''))
-                f.close()
-            except:
-                print  "Error creating Document ID: %s" % entry['id'] + '.html'
-                print  sys.exc_info()
+            # Create a dict of existing files ids, for finding deleted ones since last backup
+            file_dict = {}
+            for f in glob.glob(GROUP_DIR + '/*.html'):
+                file_dict[os.path.basename(os.path.splitext(f)[0])] = 'True'
+
+            for entry in doc:
+                # Create a .raw file, for the future when FB implements API fo doc modification
+                try:
+                    f = codecs.open(GROUP_DIR + '/' + entry['id'] + '.raw','w', encoding='UTF-8')
+                    f.write(json.dumps(entry))
+                    f.close()
+                except:
+                    print  colored("Error creating Document ID: %s" % entry['id'] + '.raw', 'FAIL')
+                    print  sys.exc_info()
+                else:
+                    print colored("Created Document ID: ", 'OK') + colored(entry['id'] + '.raw')
+
+                # Create a .html file to easy copy/paste edits of FB doc online
+                if not 'message' in entry.keys(): # FB doesn't send 'message' if Doc is empty
+                    entry['message'] = ''
+
+                try:
+                    f = codecs.open(GROUP_DIR + '/' + entry['id'] + '.html','w', encoding='UTF-8')
+                    f.write(HTML % (entry['subject'], entry['subject'], entry['message']))
+                    f.close()
+                except:
+                    print  colored("Error creating Document ID: %s" % entry['id'] + '.html', 'FAIL')
+                    print  sys.exc_info()
+                else:
+                    print colored("Created Document ID: ", 'OK') + colored(entry['id'] + '.html')
+
+                # Remove file id from the dict
+                if entry['id'] in file_dict.keys():
+                    del file_dict[entry['id']]
+
+            # Check if files are left in dict. If yes, means they were deleted from the last backup
+            if len(file_dict) == 0:
+                print colored("No Docs since last backup were deleted from this Group with ID: ", 'OK') + colored(gid)
             else:
-                print "Created Document ID: %s" % entry['id'] + '.html'
+                print colored("Found Docs deleted since last backup from this Group with ID: %s" % gid, 'FAIL')
 
-    print "\nJob done."
-    print "Do not forget to add the folder -> %s <- to your git, hg,svn etc repo\n" % SCRIPT_DIR
+                # Create a Group ID "deleted" folder to keep deleted files available
+                GROUP_DIR_DELETED = GROUP_DIR + '/deleted'
+                if not os.path.exists(GROUP_DIR_DELETED):
+                    try:
+                        os.makedirs(GROUP_DIR_DELETED)
+                    except:
+                        print  colored("Error creating folder for deleted files in Group ID: %s" % GROUP_DIR_DELETED, 'FAIL')
+                        print  sys.exc_info()
+                    else:
+                        print colored("\nCreated folder for deleted files in Group ID: ", 'OK') + colored(gid)
+
+                print colored("\nMoving all deleted files to the deleted folder of Group ID: ", 'OK') + colored(gid)
+
+                for f_deleted in file_dict.keys():
+                    data_raw = {}
+                    try:
+                        data_raw = json.loads(open(GROUP_DIR + '/' + f_deleted + '.raw').read())
+                    except:
+                        print colored("Cannot read the deleted Doc with ID: %s" % f_deleted, 'FAIL')
+                        data_raw['subject'] = 'UNKNOWN SUBJECT'
+
+                    print colored("DELETED: ", 'FAIL') + colored(f_deleted + '-> ' + data_raw['subject'])
+
+                    # Move all deleted files in GROUP_DIR_DELETED
+                    for f in glob.glob(GROUP_DIR + '/' + f_deleted + '.*'):
+                        try:
+                            shutil.move(f, GROUP_DIR_DELETED + '/' + os.path.basename(f))
+                        except:
+                            print  colored("Error moving file %s to folder: %s" % (f, GROUP_DIR_DELETED), 'FAIL')
+                            print  sys.exc_info()
+
+
+    print colored("\nJob done.", 'OK')
+    print colored("Do not forget to update your local&remote git, hg, svn etc repo\n", 'WARN')
 
 if __name__ == '__main__':
     main()
